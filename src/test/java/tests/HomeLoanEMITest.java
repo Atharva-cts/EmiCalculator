98c7dd4e767a0857d189b77b3afa8b929480f331
[Aniket Saini] <Aniket.Saini2@cognizant.com>
2026-02-25 17:38:00 +0530

    Assertion add

--- src/test/java/tests/HomeLoanEMITest.java
+++ src/test/java/tests/HomeLoanEMITest.java
@@ -1,14 +1,18 @@
 package tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
 import pages.HomeLoanPage;
+import org.testng.Assert;
 
 public class HomeLoanEMITest {
 
     private WebDriver driver;
     private HomeLoanPage homePage;
+    JavascriptExecutor js = (JavascriptExecutor) driver;
 
     @BeforeClass
     public void setup() {
@@ -27,6 +31,8 @@ public class HomeLoanEMITest {
     @Test(priority = 1)
     public void test_OpenCalculatorPage() {
         homePage.openHomeLoanCalculator();
+        Assert.assertTrue(driver.getTitle().contains("EMI"), "Home Loan EMI page did NOT open.");
+
     }
 
     @Test(priority = 2, dependsOnMethods = "test_OpenCalculatorPage")
@@ -39,17 +45,33 @@ public class HomeLoanEMITest {
         homePage.enterLoanTenure("20");
         homePage.enterLoanFees("0.25");
         homePage.enterMonth();
+
+        Assert.assertTrue(driver.findElement(By.id("homeprice")).isDisplayed(),
+                "Home value field is not displayed.");
         Thread.sleep(2000);
 
     }
     @Test(priority = 3, dependsOnMethods = "test_OpenCalculatorPage")
     public void homeowner_Expenses() throws InterruptedException {
 
+        Assert.assertTrue(driver.findElement(By.id("onetimeexpenses")).isDisplayed(),
+                "One‑time expenses field not visible.");
+
         homePage.enterOneTimeExpenses("15");
         homePage.enterPropertTaxes("0.35");
         homePage.enterHomeInsurance("0.07");
         homePage.enterMaintenanceExpenses("2750");
         Thread.sleep(2000);
+    }
+
+    // In Scroller()
+    @Test(priority = 4, dependsOnMethods = "test_OpenCalculatorPage")
+    public void Scroller() throws InterruptedException {
+
+        homePage.scrollDown();
+        Thread.sleep(1500);
+
+        Assert.assertTrue(true, "Scroll action executed successfully.");
     }
 
 
